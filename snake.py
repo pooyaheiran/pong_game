ff7f2af695dee45c0e331df791011eae0d1af431
pooyaheiran <pooyaheiran@gmail.com>
2025-03-22 00:54:15 +0330

    add keys

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((1360, 768))
 clock = pygame.time.Clock()
-
+dt = .5
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 def run_game():
@@ -16,10 +16,16 @@ def run_game():
 
         screen.fill((10,20,60))
         pygame.draw.rect(screen, "red", (player_pos.x,player_pos.y, 20, 20))
-        
+
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_pos.y -= 5
+        if keys[pygame.K_UP]:
+            player_pos.y -= 5 
+        if keys[pygame.K_DOWN]:
+            player_pos.y += 5 
+        if keys[pygame.K_RIGHT]:
+            player_pos.x += 5
+        if keys[pygame.K_LEFT]:
+            player_pos.x -= 5       
         
         
         pygame.display.flip()
