3c30e1ff5a70334a11e663ed019c640c468430f9
pooyaheiran <pooyaheiran@gmail.com>
2025-03-22 01:15:57 +0330

    update key

--- snake.py
+++ snake.py
@@ -6,30 +6,37 @@ pygame.init()
 screen = pygame.display.set_mode((1360, 768))
 clock = pygame.time.Clock()
 dt = .5
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
 
 def run_game():
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    direction = pygame.Vector2(5, 0)
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and direction.y == 0:
+                    direction = pygame.Vector2(0, -5)
+                elif event.key == pygame.K_DOWN and direction.y == 0:
+                    direction = pygame.Vector2(0, 5)
+                elif event.key == pygame.K_LEFT and direction.x == 0:
+                    direction = pygame.Vector2(-5, 0)
+                elif event.key == pygame.K_RIGHT and direction.x == 0:
+                    direction = pygame.Vector2(5, 0)
+                
 
-        screen.fill((10,20,60))
+        screen.fill((10,20,60))      
+        player_pos += direction        
+
+        
         pygame.draw.rect(screen, "red", (player_pos.x,player_pos.y, 20, 20))
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            player_pos.y -= 5 
-        if keys[pygame.K_DOWN]:
-            player_pos.y += 5 
-        if keys[pygame.K_RIGHT]:
-            player_pos.x += 5
-        if keys[pygame.K_LEFT]:
-            player_pos.x -= 5       
-        
-        
+       
+    
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(20)
 
 run_game()
 
